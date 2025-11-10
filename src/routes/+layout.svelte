c1389c5522ec4c0ea056c4bc82b7231a0f099e16
Luca Hentschel <luca.hentschel@2peaches.de>
2025-11-10 13:36:26 +0100

    add footer with GitHub link and external link icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "../app.css";
   import favicon from "$lib/assets/favicon.svg";
   import { ModeWatcher } from "mode-watcher";
+  import ExternalLink from "@lucide/svelte/icons/external-link";
 
   let { children } = $props();
 </script>
@@ -13,5 +14,16 @@
 <div class="flex h-screen w-full flex-col gap-4 p-3 sm:p-4">
   {@render children()}
 </div>
+
+<footer>
+  <a
+    href="https://github.com/The-LukeZ/reddit-downloader"
+    target="_blank"
+    class="fixed right-4 bottom-4 flex items-center gap-1 text-sm text-muted-foreground hover:underline"
+  >
+    <ExternalLink class="inline-block size-4" />
+    GitHub
+  </a>
+</footer>
 
 <ModeWatcher defaultMode="dark" />
